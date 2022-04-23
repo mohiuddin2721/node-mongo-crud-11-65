59ffa3ac1fe87d452b45cfabda760a96f08d942b
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-24 01:25:03 +0600

    delete from server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,6 +21,7 @@ async function run() {
         // const result = await userCollection.insertOne(user);
         // console.log(`User inserted with id: ${result.insertedId}`);
 
+        // get users
         app.get('/user', async (req, res) => {
             const query = {};
             const cursor = userCollection.find(query);
@@ -34,6 +35,14 @@ async function run() {
             const newUser = req.body;
             console.log('adding new user', newUser);
             const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        })
+
+        // delete a user 
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
     }
