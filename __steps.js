050ab12fbdb1504e6446f661d860b32c6dee7bb9
mohiuddin2721 <mohiuddinpatwary27@gmail.com>
2022-04-23 23:11:30 +0600

    user are added in mongodb

---
+++ __steps.js
@@ -0,0 +1,33 @@
+/**
+ * 1. Create a node server with 6 step
+ * 1.1 create folder
+ * 1.2 npm init
+ * 1.3 npm i express cors mongodb
+ * 1.4 script-dev: nodemon index.js
+ * 1.5 create index.js
+ * 1.6 use 5 step to create a node server
+ * 
+ * -------------------------------------
+ * Create atlas account
+ * -------------------------------------
+ * 1. sign up. google access
+ * 2. create cluster
+ * 3. create user dbUser and password
+ * 4. Network access --> ip address: allow all
+ * 5. database > Connect > code copy paste in index.js
+ * 
+ * -------------------------------------
+ * CRUD Operation
+ * -------------------------------------
+ * 1. node mongodb CRUD > Fundamentals
+ * 2. create async run function
+ * 
+ * -------------------------------------
+ * Integrate sending data from client to server
+ * -------------------------------------
+ * 1. Client side: create form
+ * 2. on submit get form data and create user object
+ * 3. on server: Create user POST method to receive data no the backend
+ * 4. on client side: set fetch with POST, header, body
+ * 5. make sure you return a json from the POST API
+ */
--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // use middleware
 app.use(cors());
 app.use(express.json());
+
+// connect with mongodb
+const uri = "mongodb+srv://dbuser1:cmB5o4HBdEVM7c2y@cluster0.onsg0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const userCollection = client.db("foodExpress").collection("user");
+        // const user = {name: 'Mahiya Mahi', email: 'mahi@gmail.com'};
+        // const result = await userCollection.insertOne(user);
+        // console.log(`User inserted with id: ${result.insertedId}`);
+
+        
+
+        // POST user : add a new user
+        app.post('/user', async (req, res) => {
+            const newUser = req.body;
+            console.log('adding new user', newUser);
+            const result = await userCollection.insertOne(newUser);
+            res.send(result);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Running my node CRUD server')
